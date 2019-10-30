734d1d02a64674838b9f7097e53ff2ac945a0aaf
panyh <31656197+panswork@users.noreply.github.com>
2019-10-30 14:33:07 +0800

    1

--- panswork/right.js
+++ panswork/right.js
@@ -1,37 +1,37 @@
-<ul>
-<li>
-<a href="https://www.kmway.com/myyp/821718.shtml" target="_blank" rel="nofollow" target="_blank" title="宠爱孕婴母婴店加盟">
-<img src="https://cpt.viewsrc.stream/panswork/right/OZYwFHBCfu.gif" alt="宠爱孕婴母婴店加盟" width="100" height="80">
-</a>
-</li>
-<li>
-<a href="http://www.honey-lovely.com/" target="_blank" rel="nofollow" target="_blank" title="可爱可亲加盟">
-<img src="https://cpt.viewsrc.stream/panswork/right/RKLFvdPTRS.gif" alt="可爱可亲加盟" width="100" height="80">
-</a>
-</li>
-<li>
-<a href="http://www.sohu.com/a/204564009_397401" target="_blank" rel="nofollow" target="_blank" title="华商天下荟加盟">
-<img src="https://cpt.viewsrc.stream/panswork/right/ZOCcjWWxZL.gif" alt="华商天下荟加盟" width="100" height="80">
-</a>
-</li>
-<li>
-<a href="http://www.campanilechina.com/" target="_blank" rel="nofollow" target="_blank" title="康铂酒店加盟">
-<img src="https://cpt.viewsrc.stream/panswork/right/hWihTtSRMY.gif" alt="康铂酒店加盟" width="100" height="80">
-</a>
-</li>
-<li>
-<a href="https://www.zhangmen.com/" target="_blank" rel="nofollow" target="_blank" title="掌门1对1加盟">
-<img src="https://cpt.viewsrc.stream/panswork/right/LKCcVYpwXE.gif" alt="掌门1对1加盟" width="100" height="80">
-</a>
-</li>
-<li>
-<a href="https://www.3158.cn/xiangmu/602069/" target="_blank" rel="nofollow" target="_blank" title="多多熊少儿艺术培训加盟">
-<img src="https://cpt.viewsrc.stream/panswork/right/ImkOVJKVti.gif" alt="多多熊少儿艺术培训加盟" width="100" height="80">
-</a>
-</li>
-<li>
-<a href="http://www.zsmz.com/" target="_blank" rel="nofollow" target="_blank" title="掌上明珠家具加盟">
-<img src="https://cpt.viewsrc.stream/panswork/right/fRcGCKhnAf.gif" alt="掌上明珠家具加盟" width="100" height="80">
-</a>
-</li>
-</ul>
+document.writeln("<ul>");
+document.writeln("<li>");
+document.writeln("<a href=\'https://www.kmway.com/myyp/821718.shtml\' target=\'_blank\' rel=\'nofollow\' target=\'_blank\' title=\'宠爱孕婴母婴店加盟\'>");
+document.writeln("<img src=\'https://cpt.viewsrc.stream/panswork/right/OZYwFHBCfu.gif\' alt=\'宠爱孕婴母婴店加盟\' width=\'100\' height=\'80\'>");
+document.writeln("</a>");
+document.writeln("</li>");
+document.writeln("<li>");
+document.writeln("<a href=\'http://www.honey-lovely.com/\' target=\'_blank\' rel=\'nofollow\' target=\'_blank\' title=\'可爱可亲加盟\'>");
+document.writeln("<img src=\'https://cpt.viewsrc.stream/panswork/right/RKLFvdPTRS.gif\' alt=\'可爱可亲加盟\' width=\'100\' height=\'80\'>");
+document.writeln("</a>");
+document.writeln("</li>");
+document.writeln("<li>");
+document.writeln("<a href=\'http://www.sohu.com/a/204564009_397401\' target=\'_blank\' rel=\'nofollow\' target=\'_blank\' title=\'华商天下荟加盟\'>");
+document.writeln("<img src=\'https://cpt.viewsrc.stream/panswork/right/ZOCcjWWxZL.gif\' alt=\'华商天下荟加盟\' width=\'100\' height=\'80\'>");
+document.writeln("</a>");
+document.writeln("</li>");
+document.writeln("<li>");
+document.writeln("<a href=\'http://www.campanilechina.com/\' target=\'_blank\' rel=\'nofollow\' target=\'_blank\' title=\'康铂酒店加盟\'>");
+document.writeln("<img src=\'https://cpt.viewsrc.stream/panswork/right/hWihTtSRMY.gif\' alt=\'康铂酒店加盟\' width=\'100\' height=\'80\'>");
+document.writeln("</a>");
+document.writeln("</li>");
+document.writeln("<li>");
+document.writeln("<a href=\'https://www.zhangmen.com/\' target=\'_blank\' rel=\'nofollow\' target=\'_blank\' title=\'掌门1对1加盟\'>");
+document.writeln("<img src=\'https://cpt.viewsrc.stream/panswork/right/LKCcVYpwXE.gif\' alt=\'掌门1对1加盟\' width=\'100\' height=\'80\'>");
+document.writeln("</a>");
+document.writeln("</li>");
+document.writeln("<li>");
+document.writeln("<a href=\'https://www.3158.cn/xiangmu/602069/\' target=\'_blank\' rel=\'nofollow\' target=\'_blank\' title=\'多多熊少儿艺术培训加盟\'>");
+document.writeln("<img src=\'https://cpt.viewsrc.stream/panswork/right/ImkOVJKVti.gif\' alt=\'多多熊少儿艺术培训加盟\' width=\'100\' height=\'80\'>");
+document.writeln("</a>");
+document.writeln("</li>");
+document.writeln("<li>");
+document.writeln("<a href=\'http://www.zsmz.com/\' target=\'_blank\' rel=\'nofollow\' target=\'_blank\' title=\'掌上明珠家具加盟\'>");
+document.writeln("<img src=\'https://cpt.viewsrc.stream/panswork/right/fRcGCKhnAf.gif\' alt=\'掌上明珠家具加盟\' width=\'100\' height=\'80\'>");
+document.writeln("</a>");
+document.writeln("</li>");
+document.writeln("</ul>");
