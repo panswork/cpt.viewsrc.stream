0a4f3ab9b79f5d1099f43651f8ff81f69b5b03fd
panyh <31656197+panswork@users.noreply.github.com>
2019-10-30 15:17:35 +0800

    1

---
+++ panswork/content.js
@@ -0,0 +1,9 @@
+document.writeln("<ul>");
+document.writeln("<li>");
+document.writeln("<a  href=\'http://www.sdqwx.com/index.html\' target=\'_blank\' rel=\'nofollow\'  target=\'_blank\' title=\'泉味轩豆腐脑加盟\'><img src=\'https://cpt.viewsrc.stream/panswork/content/QhHtRFejcv.gif\' alt=\'泉味轩豆腐脑加盟\' width=\'930px\' height=\'70\'></a>");
+document.writeln("</li>");
+document.writeln("<li>");
+document.writeln("<a  href=\'http://www.anzon.cn/\' target=\'_blank\' rel=\'nofollow\'  target=\'_blank\' title=\'汉逊集成热水器加盟\'><img src=\'https://cpt.viewsrc.stream/panswork/content/ETxNjtlXXz.gif\' alt=\'汉逊集成热水器加盟\' width=\'930px\' height=\'70\'></a>");
+document.writeln("</li>");
+document.writeln("");
+document.writeln("</ul>");
