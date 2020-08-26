08a4eb27dc9600502edff5dad606a6627d808122
panyh <31656197+panswork@users.noreply.github.com>
2020-08-26 21:47:47 +0800

    1

--- gene/gene.js
+++ gene/gene.js
@@ -1,1 +1,1 @@
-document.writeln("<a href=\'https://www.bioosys.com/about.html\' target=\'_blank\'><img src=\'https://cpt.viewsrc.stream/gene/test.jpg\' style=\'width:240px\'/></a><br /><br />");
+document.writeln("<a href=\'https://www.bioosys.com/about.html\' target=\'_blank\'><img src=\'https://cpt.viewsrc.stream/gene/test.jpg\' style=\'width:220px\'/></a><br /><br />");
